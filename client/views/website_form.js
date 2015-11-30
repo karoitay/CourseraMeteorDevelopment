a3dde64d12e06eae56d147945bcd89598f73b054
Itay Karo <karoitay@gmail.com>
2015-12-01 00:16:51 +0200

    allow auto meradata fetching when adding sites

--- client/views/website_form.js
+++ client/views/website_form.js
@@ -7,10 +7,37 @@ var isValid = function(field, value) {
 };
 
 Template.website_form.events({
-	"click .js-toggle-website-form":function(event) {
+	"click .js-toggle-website-form": function(event) {
 		$("#website_form").toggle('slow');
-	}, 
-	"submit .js-save-website-form":function(event) {
+	},
+	"input, change, paste, keyup, mouseup #url": function(event) {
+		if (event.target.value.trim() === "") {
+			$(".js-get-metadata-button").attr('disabled','disabled');
+		} else {
+			$(".js-get-metadata-button").removeAttr("disabled");
+		}
+	},
+	"click .js-get-metadata-button": function(event) {
+		$("#website_form :input").attr("disabled", true);
+		var form = $("#website_form");
+		console.log(form);
+		console.log(form.find("#url").val().trim());
+		extractMeta(form.find("#url").val(), function(err, res) {
+			$("#website_form :input").attr("disabled", false);
+			if (err) {
+				alert(err);
+			} else {
+				console.log(res);
+				if (res.title) {
+					form.find("#title").val(res.title);
+				}
+				if (res.description) {
+					form.find("#description").val(res.description);
+				}
+			}
+		});
+	},
+	"submit .js-save-website-form": function(event) {
 		var url = event.target.url.value;
 		var title = event.target.title.value;
 		var description = event.target.description.value;
@@ -28,6 +55,9 @@ Template.website_form.events({
 			if (err) {
 				alert(err);
 			} else {
+				event.target.url.value = "";
+				event.target.title.value = "";
+				event.target.description.value = "";
 				$("#website_form").toggle('slow');
 			}
 		});
