11331caee17c354a381912d0e0a46e091e1dcef9
Itay Karo <karoitay@gmail.com>
2015-12-02 05:17:30 +0200

    set active navbar class even when in details page

--- client/views/siteaces.js
+++ client/views/siteaces.js
@@ -6,10 +6,14 @@ Router.route('/', function () {
  	this.render('mainPage');
 });
 
+Router.route('/site/:_id', function () {
+	this.render('siteDetails', {data: Websites.findOne({_id: this.params._id})});
+});
+
 Router.route('/recommended', function () {
  	this.render('mainPage');
 });
 
-Router.route('/site/:_id', function () {
+Router.route('/recommended/site/:_id', function () {
 	this.render('siteDetails', {data: Websites.findOne({_id: this.params._id})});
 });
